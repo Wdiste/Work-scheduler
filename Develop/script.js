86f10b81a892cf4194b167860ac89ba75f48dd60
wdiste <Wdiste49@gmail.com>
2022-12-24 17:36:59 -0500

    skeleton for HTML complete and todays date saved/displayed

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+  // shorthand for $(document).ready(function(args){...})
 $(function () {
+  // set up current day for display in header format: (Jan 1, 2022)
+  var today = dayjs();
+  $('#currentDay').text(today.format('MMM D, YYYY'));
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
